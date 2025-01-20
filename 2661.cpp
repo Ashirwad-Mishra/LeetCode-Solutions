79187c30926c696365f0518f1a4268ddd3414816
Ashirwad Mishra <146855394+Ashirwad-Mishra@users.noreply.github.com>
2025-01-20 20:30:03 +0530

    Update 2661.cpp

--- 2661.cpp
+++ 2661.cpp
@@ -29,3 +29,35 @@ public:
         return 0;
     }
 };
+
+// Above method was absolutely right. But, still dont use this method as it uses map to store the cordinates of the numbers. Through, above written logic you will end up taking 600 ms.
+// instead use a vector of a pair as given below , through below written logic it shall only take 4ms.
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        vector <pair <int , int>> LUT(m*n+1);
+        for (int i = 0 ; i < m ; i++)
+        {
+            for (int j = 0 ; j < n ; j++)
+            {
+                int t = mat[i][j] ; 
+                LUT[t] = make_pair(i,j);
+            }
+        }
+        vector <int> rC(m , 0);
+        vector <int> cC(n , 0);
+        for (int i = 0 ; i < arr.size() ; i++)
+        {
+            int t = arr[i];
+            int r = LUT[t].first;
+            rC[r]++;
+            if (rC[r] == n) return i;
+            int c = LUT[t].second;
+            cC[c]++;
+            if (cC[c] == m) return i;
+        }
+        return 0;
+    }
+};

d68155d90b044e41d82cb3b3487bbf92f5be4a86
Ashirwad Mishra <146855394+Ashirwad-Mishra@users.noreply.github.com>
2025-02-11 03:10:56 +0530

    Update 51.cpp

--- 51.cpp
+++ 51.cpp
@@ -1,3 +1,6 @@
+// Approach 1 :-
+
+
 class Solution {
 private:
     bool isValid(vector <string> &board , int row , int col , int n)
@@ -39,6 +42,87 @@ public:
         vector <vector <string>> ans;
         vector <string> board(n , string(n , '.'));
         solve (ans , 0 , n , board);
+        return ans;
+    }
+};
+
+// Aproach 2:- 
+class Solution {
+private:
+    void solve(vector <vector <string>> &ans , int row , int n , vector <string> &board , unordered_set <int> &cols , unordered_set <int> &D , unordered_set <int> &AD)
+    {
+        if (row >= n)
+        {
+            ans.push_back(board);
+            return;
+        }
+        for (int col = 0 ; col < n ; col++)
+        {
+            int DC = row + col;
+            int ADC = row - col;
+            if (cols.find(col) != cols.end() || D.find(DC) != D.end() || AD.find(ADC) != AD.end())
+            {
+                continue;
+            }
+            cols.insert(col);
+            D.insert(DC);
+            AD.insert(ADC);
+            board[row][col] = 'Q';
+            solve (ans , row + 1, n , board , cols , D , AD);
+            board[row][col] = '.';
+            cols.erase(col);
+            D.erase(DC);
+            AD.erase(ADC);
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector <vector <string>> ans;
+        vector <string> board(n , string(n , '.'));
+        unordered_set <int> cols;
+        unordered_set <int> D;
+        unordered_set <int> AD;
+        solve (ans , 0 , n , board , cols , D , AD);
+        return ans;
+    }
+};
+// Approach 3 :- Best Approach
+class Solution {
+private:
+    void solve(vector <vector <string>> &ans , int row , int n , vector <string> &board , vector <bool> &cols , vector <bool> &D , vector <bool> &AD)
+    {
+        if (row >= n)
+        {
+            ans.push_back(board);
+            return;
+        }
+        for (int col = 0 ; col < n ; col++)
+        {
+            int DC = row + col;
+            int ADC = row - col + n - 1;
+            if (cols[col] || D[DC] || AD[ADC])
+            {
+                continue;
+            }
+            cols[col] = true;
+            D[DC] = true;
+            AD[ADC] = true;
+            board[row][col] = 'Q';
+            solve (ans , row + 1, n , board , cols , D , AD);
+            board[row][col] = '.';
+            cols[col] = false;
+            D[DC] = false;
+            AD[ADC] = false;
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector <vector <string>> ans;
+        vector <string> board(n , string(n , '.'));
+        vector <bool> cols(n, false);
+        vector <bool> D(3*n, false);
+        vector <bool> AD(3*n, false);
+        solve (ans , 0 , n , board , cols , D , AD);
         return ans;
     }
 };

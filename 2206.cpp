48b39a05aa2a2a678aa92f27c3f1aa25eed89554
Ashirwad Mishra <146855394+Ashirwad-Mishra@users.noreply.github.com>
2025-03-17 08:23:24 +0530

    Create 2206.cpp
    https://leetcode.com/problems/divide-array-into-equal-pairs/?envType=daily-question&envId=2025-03-17

---
+++ 2206.cpp
@@ -0,0 +1,17 @@
+// with map 
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map <int,int>m;
+        for (auto i : nums) 
+        {
+            m[i]++;
+        }
+        for (auto &p : m)
+        {
+            if (p.second % 2 != 0) return false;
+        }
+        return true;
+    }
+};
+

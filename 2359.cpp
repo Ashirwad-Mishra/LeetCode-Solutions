b23f6b8d3653df0ca90292f65e7a24c9db72c50d
Ashirwad Mishra <146855394+Ashirwad-Mishra@users.noreply.github.com>
2025-05-31 00:38:53 +0530

    Create 2359.cpp
    https://leetcode.com/problems/find-closest-node-to-given-two-nodes/?envType=daily-question&envId=2025-05-30

---
+++ 2359.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        vector <int> vec1(edges.size() , INT_MAX);
+        unordered_map <int , vector<int>> mp;
+        for (int i = 0 ; i < edges.size() ; i++)
+        {
+            if (edges[i] == -1) continue;
+            mp[i].push_back(edges[i]);
+        }
+
+        priority_queue <pair <int , int> , vector <pair<int , int>> , greater <pair <int , int>>> minHeap;
+
+        minHeap.push({0 , node1});
+        vec1[node1] = 0;
+        while(!minHeap.empty())
+        {
+            int root = minHeap.top().second;
+            int wt = minHeap.top().first;
+            minHeap.pop();
+            for (auto &i : mp[root])
+            {
+                if (vec1[i] > wt + 1)
+                {
+                    minHeap.push({wt + 1 , i});
+                    vec1[i] = min(wt + 1 , vec1[i]);
+                }
+            }
+        }
+        vector <int> vec2(edges.size() , INT_MAX);
+        priority_queue <pair <int , int> , vector <pair<int , int>> , greater <pair <int , int>>> Heap;
+        Heap.push({0 , node2});
+        vec2[node2] = 0;
+        while(!Heap.empty())
+        {
+            int root = Heap.top().second;
+            int wt = Heap.top().first;
+            Heap.pop();
+            for (auto &i : mp[root])
+            {
+                if (vec2[i] > wt + 1)
+                {
+                    Heap.push({wt + 1 , i});
+                    vec2[i] = min(wt + 1 , vec2[i]);
+                }
+            }
+        }
+
+        int ans = -1, minDist = INT_MAX;
+        for (int i = 0; i < edges.size(); i++) 
+        {
+            if (vec1[i] != INT_MAX && vec2[i] != INT_MAX) 
+            {
+                int maxDist = max(vec1[i], vec2[i]);
+                if (maxDist < minDist ) 
+                {
+                    minDist = maxDist;
+                    ans = i;
+                }
+            }
+        }
+
+        return ans; 
+    }
+};
